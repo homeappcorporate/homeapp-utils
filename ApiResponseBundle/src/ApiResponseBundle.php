00d40e7c1deb2fbd3f9adef7ddc4abd9ffb30074
Alexey Prizov <alex.deel@gmail.com>
2019-05-24 15:40:43 +0300

    Add dependency injection

--- ApiResponseBundle/src/ApiResponseBundle.php
+++ ApiResponseBundle/src/ApiResponseBundle.php
@@ -2,8 +2,13 @@
 
 namespace ApiResponseBundle;
 
+use ApiResponseBundle\DependencyInjection\ApiResponseExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ApiResponseBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new ApiResponseExtension();
+    }
 }
