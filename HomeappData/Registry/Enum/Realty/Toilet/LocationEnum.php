b70bb7b5450fa82a7581167b9c2d306dc4aac6f5
David Bagarian <dave.bagaron@yandex.ru>
2021-01-12 19:35:29 +0300

    removed some registry stuff + refactored exception name

--- HomeappData/Registry/Enum/Realty/Toilet/LocationEnum.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Homeapp\HomeappData\Registry\Enum\Realty\Toilet;
-
-class LocationEnum
-{
-    public const INSIDE = 'inside';
-    public const OUTSIDE = 'outside';
-}
