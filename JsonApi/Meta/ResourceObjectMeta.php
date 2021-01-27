a211c57606b90301f65b9c9276e5b3b227be8ffb
Konstantin Polyakov <konspolyakov@gmail.com>
2021-01-27 18:28:23 +0300

    [PHP-1035]: Add meta for ResourceObject

--- JsonApi/Meta/ResourceObjectMeta.php
+++ JsonApi/Meta/ResourceObjectMeta.php
@@ -5,34 +5,19 @@ declare(strict_types=1);
 namespace Homeapp\JsonApi\Meta;
 
 use DateTimeImmutable;
-use JMS\Serializer\Annotation as Serializer;
 
-/**
- * Весь блок 'meta' целиком
- */
-class Meta
+class ResourceObjectMeta
 {
-    /**
-     * @var MetaPage|null
-     * @Serializer\Type("Homeapp\JsonApi\Meta\MetaPage")
-     * @Serializer\Groups({"API"})
-     */
-    public ?MetaPage $page = null;
-
     /**
      * @var MetaConsistencyItem[]
      * @Serializer\Type("array<Homeapp\JsonApi\Meta\MetaConsistencyItem>")
      * @Serializer\Groups({"API"})
-     *
-     * @deprecated перенесено в ResourceObjectMeta
      */
     public ?array $consistency = null;
 
     /**
      * @Serializer\Groups({"API"})
      * @Serializer\Type("DateTimeImmutable<'Y-m-d H:i:s'>")
-     *
-     * @deprecated перенесено в ResourceObjectMeta
      */
     public ?DateTimeImmutable $updatedAt = null;
 }
