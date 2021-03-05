9ee5ceaa0e122d5094a61e7bac4a3d27bb717562
EvgeniiR <evgromr1@gmail.com>
2021-03-05 14:23:39 +0300

    Update metadata

--- HomeappData/Registry/Dto/Complex/Data.php
+++ HomeappData/Registry/Dto/Complex/Data.php
@@ -5,22 +5,19 @@ declare(strict_types=1);
 namespace Homeapp\HomeappData\Registry\Dto\Complex;
 
 use Homeapp\HomeappData\Registry\Dto\ResourceObject;
-use Homeapp\JsonApi\Attributes\AttributesInterface;
 use Homeapp\HomeappData\Registry\Enum\EntityNameEnum;
 use JMS\Serializer\Annotation as Serializer;
 
-/**
- * @property-read null|Attributes $attributes;
- */
 class Data extends ResourceObject
 {
     /**
+     * @property-read
      * @Serializer\Type("Homeapp\HomeappData\Registry\Dto\Complex\Attributes")
      * @Serializer\Groups({"API"})
      */
-    public ?AttributesInterface $attributes = null;
+    public ?Attributes $attributes = null;
 
-    public function __construct(?string $id, ?AttributesInterface $attributes)
+    public function __construct(?string $id, ?Attributes $attributes)
     {
         parent::__construct($id, EntityNameEnum::COMPLEX);
         $this->attributes = $attributes;
--- HomeappData/Registry/Dto/Image/Data.php
+++ HomeappData/Registry/Dto/Image/Data.php
@@ -5,22 +5,19 @@ declare(strict_types=1);
 namespace Homeapp\HomeappData\Registry\Dto\Image;
 
 use Homeapp\HomeappData\Registry\Dto\ResourceObject;
-use Homeapp\JsonApi\Attributes\AttributesInterface;
 use Homeapp\HomeappData\Registry\Enum\EntityNameEnum;
 use JMS\Serializer\Annotation as Serializer;
 
-/**
- * @property-read null|Attributes $attributes;
- */
 class Data extends ResourceObject
 {
     /**
+     * @property-read
      * @Serializer\Type("Homeapp\HomeappData\Registry\Dto\Image\Attributes")
      * @Serializer\Groups({"API"})
      */
-    public ?AttributesInterface $attributes = null;
+    public ?Attributes $attributes = null;
 
-    public function __construct(?string $id, ?AttributesInterface $attributes)
+    public function __construct(?string $id, ?Attributes $attributes)
     {
         parent::__construct($id, EntityNameEnum::IMAGE);
         $this->attributes = $attributes;
