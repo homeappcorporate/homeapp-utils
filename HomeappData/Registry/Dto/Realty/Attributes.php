80fdf064231e3794672ee5ab2c306704943d4f1d
Alexey Prizov <alex.deel@gmail.com>
2022-03-28 14:52:14 +0300

    PHP-1824: add isPresentationReady field;

--- HomeappData/Registry/Dto/Realty/Attributes.php
+++ HomeappData/Registry/Dto/Realty/Attributes.php
@@ -444,6 +444,12 @@ class Attributes implements AttributesInterface
      */
     public ?bool $burden;
 
+    /**
+     * @Serializer\Type("boolean")
+     * @Serializer\Groups({"API"})
+     */
+    public ?bool $isPresentationReady;
+
     public function __construct(
         string $realtyType,
         float $area,
